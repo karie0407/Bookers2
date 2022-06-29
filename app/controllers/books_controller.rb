fee2b8c49bf9cd054e04c95cbf21f143652c67b8
karie0407 <karie0407@users.noreply.github.com>
2022-06-29 14:07:51 +0000

    [add]課題　　ユーザーページ編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,7 +12,8 @@ class BooksController < ApplicationController
   
   def index
     @books = Book.all
-    @book =Book.new
+    
+    
   end
 
   def show
@@ -20,6 +21,13 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book =Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
   
   
